4c1de00a9dc0455fcfe883f823232f048fd85941
Eoin O Toole <S00177807@mail.itsligo.ie>
2020-02-06 21:45:46 +0000

    commit angulad project

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {Article} from './article/article.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  articles:Article[];
+
+constructor(){
+  this.articles=[
+    new Article('Angular','http://angular.io',5),
+    new Article('Google','http://google.ie',3),
+    new Article('TikTok','http://tiktok.com',1)
+
+  ];
+}
+
+  addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean{
+    console.log(`Adding Article Title: ${title.value} and adding Article Link ${link.value} :`);
+    return false;
+  }
+}
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  article:Article;
+
+  constructor() {
+    //this.article= new Article("Angular","http://angular.io", 10)
+    
+   }
+
+  ngOnInit() {
+
+  }
+
+  voteup():boolean {
+this.article.voteup();
+return false;
+  }
+
+  votedown():boolean {
+this.article.votedown();
+return false;
+  }
+
+}
